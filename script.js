f10dfd1e34165fbd91492d1bc04092b6b270e517
Vyacheslav Shakhov <slava.shaxov.98@mail.ru>
2023-06-03 20:09:19 +0300

    Add cycle

--- script.js
+++ script.js
@@ -8,10 +8,30 @@ const personalMovieDB = {
     privat: false,
 };
 
-let q1 = prompt('Один из последних просмотренных фильмов?');
-let q2 = +prompt('На сколько оцените его?');
-personalMovieDB.movies[q1] = q2;
+// for (let x = 0; x <= 2; x++) {
+//     let q1 = prompt('Один из последних просмотренных фильмов?');
+//     if (q1 == '' || q1 == null || q1.length > 50) {
+//         q1 = prompt('Один из последних просмотренных фильмов?');
+//     }
+//     let q2 = +prompt('На сколько оцените его?');
+//     personalMovieDB.movies[q1] = q2;
+// }
 
-q1 = prompt('Один из последних просмотренных фильмов?');
-q2 = +prompt('На сколько оцените его?');
-personalMovieDB.movies[q1] = q2;
+for (let x = 0; x < 2; x++) {
+    let q1 = prompt('Один из последних просмотренных фильмов?');
+
+    while(true) {
+        if (q1 == '' || q1 == null || q1.length > 50) {
+            q1 = prompt('Один из последних просмотренных фильмов?');
+        } else break
+    }
+
+    let q2 = +prompt('На сколько оцените его?');
+    personalMovieDB.movies[q1] = q2;
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else alert('Произошла ошибка');
