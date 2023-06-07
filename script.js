0538874efeb6854e934f1d3cbe901c06277af072
Vyacheslav Shakhov <slava.shaxov.98@mail.ru>
2023-06-07 12:12:19 +0300

    Add functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,30 +18,93 @@ const personalMovieDB = {
     privat: false,
 };
 
-// for (let x = 0; x <= 2; x++) {
-//     let q1 = prompt('Один из последних просмотренных фильмов?');
-//     if (q1 == '' || q1 == null || q1.length > 50) {
-//         q1 = prompt('Один из последних просмотренных фильмов?');
-//     }
-//     let q2 = +prompt('На сколько оцените его?');
-//     personalMovieDB.movies[q1] = q2;
-// }
-
-for (let x = 0; x < 2; x++) {
-    let q1 = prompt('Один из последних просмотренных фильмов?');
-
-    while(true) {
-        if (q1 == '' || q1 == null || q1.length > 50) {
-            q1 = prompt('Один из последних просмотренных фильмов?');
-        } else break
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let q1 = prompt('Один из последних просмотренных фильмов?');
+        let q2 = prompt('На сколько оцените его?');
+        
+        if (q1 == '' || q2 == '' || q1 == null || q2 == null || q1.length > 50) {
+            i--;
+            console.log('False');
+        } else {
+            personalMovieDB.movies[q1] = q2;
+            console.log('Done');
+        }
     }
-
-    let q2 = +prompt('На сколько оцените его?');
-    personalMovieDB.movies[q1] = q2;
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else alert('Произошла ошибка');
+rememberMyFilms();
+
+function detectPersolnalLevels() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 10) {
+        alert ('Вы киноман!');
+    } else alert('Произошла ошибка');
+}
+
+detectPersolnalLevels();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
+    }
+}
+
+writeYourGenres();
+
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     console.log('First level: ' + i);
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//         console.log('Second level: ' + j);
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//         console.log('Third level: ' + i);
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
+// const line = 3;
+// let res = '';
+
+// //Основной цикл задает переносы
+// for (let i = 0; i <= line; i++) {
+//     //Первый вложенный цикл делает пробелы
+//     for (let j = 0; j <= line - i; j++) {
+//         res += ' '
+//     }
+//     //Второй вложенный цикл делает звездочки
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         res += '*'
+//     }
+//     res += '\n'
+// }
+
+// console.log(res);
+
+
+let str = 'Hello, world!'
+let world
+
+console.log(str.length);
+console.log(world = str.slice(7,12).toUpperCase());
+console.log(world.toLowerCase().indexOf('ld'));
